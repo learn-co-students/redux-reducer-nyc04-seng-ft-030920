6b3902561e354e209682378b3dd6af68c0c47535
isabel k. lee <hello@isabelklee.co>
2020-06-01 15:42:43 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+let initialState = {
+  friends: []
+}
+
+export function manageFriends(state = initialState, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [
+          ...state.friends,
+          action.friend
+        ]
+      }
+    case "REMOVE_FRIEND":
+      let findIndex = state.friends.findIndex(friend => friend.id === action.id)
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, findIndex),
+          ...state.friends.slice(findIndex + 1)
+        ]
+      }
+    default:
+      return state
+  }
 }
